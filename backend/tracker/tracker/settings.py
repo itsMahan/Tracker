3668eafe896ea0f5583369deb4eda519e9864547
itsMahan <ataeifarmahan@gmail.com>
2025-09-25 07:56:07 -0400

    fix CORS

--- backend/tracker/tracker/settings.py
+++ backend/tracker/tracker/settings.py
@@ -44,9 +44,12 @@ INSTALLED_APPS = [
     #3rd-party
     'rest_framework',
     'drf_spectacular',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +58,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'tracker.urls'
 
