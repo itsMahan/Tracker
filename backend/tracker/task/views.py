106d5de91988d709c5ddd2ca6c585813c167c91c
itsMahan <ataeifarmahan@gmail.com>
2025-09-25 06:05:09 -0400

    create task (POST)

--- backend/tracker/task/views.py
+++ backend/tracker/task/views.py
@@ -14,3 +14,14 @@ class TaskListView(APIView):
         tasks = Task.objects.all()
         serializer = TaskSerializer(tasks, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class TaskCreateView(APIView):
+    serializer_class = TaskSerializer
+
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
