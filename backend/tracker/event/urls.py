452135b40d0e27b426f080ce3e27aa345d0b28cd
itsMahan <ataeifarmahan@gmail.com>
2025-10-10 07:05:37 -0400

    add event reset button

--- backend/tracker/event/urls.py
+++ backend/tracker/event/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'event'
 urlpatterns = [
     path('list', views.EventListView.as_view(), name="list_events"),
-    path('add', views.EventCreateView.as_view(), name="create_events"),
-    path('update/<int:pk>', views.EventUpdateView.as_view(), name="update_events"),
-    path('delete/<int:pk>', views.EventDeleteView.as_view(), name="delete_events"),
+    path('add', views.EventCreateView.as_view(), name="create_event"),
+    path('update/<int:pk>', views.EventUpdateView.as_view(), name="update_event"),
+    path('delete/<int:pk>', views.EventDeleteView.as_view(), name="delete_event"),
+    path('reset/<int:pk>', views.EventResetView.as_view(), name="reset_event"),
 ]
--- backend/tracker/event/views.py
+++ backend/tracker/event/views.py
@@ -1,5 +1,7 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .serializers import EventSerializer
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from .models import Event
 from task.permissions import IsUserOrReadOnly
@@ -34,3 +36,18 @@ class EventDeleteView(generics.DestroyAPIView):
     queryset = Event.objects.all()
     permission_classes = [IsAuthenticated, IsUserOrReadOnly]
 
+
+class EventResetView(APIView):
+    serializer_class = EventSerializer
+    permission_classes = [IsAuthenticated, IsUserOrReadOnly]
+
+    def patch(self, request, pk):
+        try:
+            counter = Event.objects.get(id=pk)
+        except Event.DoesNotExist:
+            return Response({'error': 'Counter Not Found!'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = EventSerializer(counter)
+        counter.used = 1
+        counter.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
