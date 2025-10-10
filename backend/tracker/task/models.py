43bc3b45deedea75232d22445f0394f7fec56d1e
itsMahan <ataeifarmahan@gmail.com>
2025-10-10 04:57:13 -0400

    add Task Mark as Done/UnDone Button

--- backend/tracker/task/models.py
+++ backend/tracker/task/models.py
@@ -8,6 +8,7 @@ class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    is_done = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
