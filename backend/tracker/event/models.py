eb92858db3043fd8e8639ac749b4a21bbf3743b0
itsMahan <ataeifarmahan@gmail.com>
2025-10-11 10:48:38 -0400

    event default/reset button set to 0

--- backend/tracker/event/models.py
+++ backend/tracker/event/models.py
@@ -8,7 +8,7 @@ class Event(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='events')
     title = models.CharField(max_length=255)
     total = models.IntegerField(null=True, blank=True, )
-    used = models.IntegerField(default=1)
+    used = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
