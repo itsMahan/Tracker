766f326eccb9961c695d98d2771ec7c37456f8dc
mahan <ataeifarmahan@gmail.com>
2026-01-07 17:07:22 +0330

    removed this shitty admin.py codes

--- backend/tracker/accounts/admin.py
+++ backend/tracker/accounts/admin.py
@@ -1,31 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-
 from .models import CustomUser, Profile
 
-# Register your models here.
-
-@admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    list_display = ("email", "is_staff", "is_active", "is_verified")
-    # list_filter = ()
-    ordering = ("email",)
-    search_fields = ("first_name", "last_name")
-
-    fieldsets = (
-        (None, {"fields": ("email", "password")}),
-        ("Personal info", {"fields": ("first_name", "last_name")}),
-        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
-        ("Important dates", {"fields": ("last_login", "date_joined")}),
-    )
-
-    add_fieldsets = (
-        (None, {
-            "classes": ("wide",),
-            "fields": ("email", "first_name", "last_name", "password1", "password2", "is_staff", "is_active"),
-        }),
-    )
 
 
+admin.site.register(CustomUser)
 admin.site.register(Profile)
