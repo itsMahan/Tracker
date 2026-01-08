8c77c1bda597696e5cceafebd05e11136e74cda8
mahan <ataeifarmahan@gmail.com>
2026-01-08 15:45:18 +0330

    use Profile instetad of User as Foreign key for models

--- backend/tracker/counter/models.py
+++ backend/tracker/counter/models.py
@@ -1,15 +1,15 @@
 from django.db import models
-from django.conf import settings
 from datetime import date
 from django.utils.timezone import now
 from django.core.exceptions import ValidationError
+# from ..accounts.models import Profile
 
 
 # Create your models here.
 
 
 class Counter(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='counters')
+    user = models.ForeignKey('accounts.Profile', on_delete=models.CASCADE, related_name='counters')
     title = models.CharField(max_length=255)
     start_date = models.DateField(default=date.today)
     created_at = models.DateField(auto_now_add=True)
